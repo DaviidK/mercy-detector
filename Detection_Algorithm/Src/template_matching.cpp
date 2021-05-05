a4c07c3ef50eb009e993dc6f3a97f26077252ef0
Sana Suse <sana96@uw.edu>
2021-05-05 16:15:23 -0700

    Update template_matching.cpp

--- Detection_Algorithm/Src/template_matching.cpp
+++ Detection_Algorithm/Src/template_matching.cpp
@@ -7,25 +7,17 @@
 using namespace std;
 using namespace cv;
 
-bool use_mask;
-Mat img; Mat templ; Mat mask; Mat result;
+Mat img; Mat templ; Mat result;
 const char* image_window = "Source Image";
 const char* result_window = "Result window";
-const char* image_name = "C:/Users/12068/Documents/GitHub/mercy-detector/Detection_Algorithm/Data/Static_Test_Im/busy.jpg";
-const char* template_name = "C:/Users/12068/Documents/GitHub/mercy-detector/Detection_Algorithm/Data/Static_Test_Im/template.jpg";
+const char* image_name = "Detection_Algorithm/Data/Static_Test_Im/busy.jpg";
+const char* template_name = "Detection_Algorithm/Data/Static_Test_Im/template.jpg";
 int match_method;
 int max_Trackbar = 5;
 
 void MatchingMethod(int, void*);
 
 int main(int argc, char** argv) {
-    const char* partialPath = "Detection_Algorithm/Data/Static_Test_Im/busy.jpg";
-    char full[_MAX_PATH];
-    if (_fullpath(full, partialPath, _MAX_PATH) != NULL)
-        printf("Full path is: %s\n", full);
-    else
-        printf("Invalid path\n");
-    
     img = imread(image_name, IMREAD_COLOR);
     templ = imread(template_name, IMREAD_COLOR);
 
@@ -48,18 +40,16 @@ int main(int argc, char** argv) {
 void MatchingMethod(int, void*) {
     Mat img_display;
     img.copyTo(img_display);
-    int result_cols = img.cols - templ.cols + 1;
-    int result_rows = img.rows - templ.rows + 1;
-    result.create(result_rows, result_cols, CV_32FC1);
-    bool method_accepts_mask = (TM_SQDIFF == match_method || match_method == TM_CCORR_NORMED);
-
+    int result_cols = img.cols; // - templ.cols + 1
+    int result_rows = img.rows; // - templ.rows + 1
+    result.create(result_rows, result_cols, COLOR_BGR2GRAY); // , CV_32FC1
     matchTemplate(img, templ, result, match_method);
 
     normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
     double minVal; double maxVal; Point minLoc; Point maxLoc;
     Point matchLoc;
     minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-
+    
     if (match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED) {
         matchLoc = minLoc;
     } else {
