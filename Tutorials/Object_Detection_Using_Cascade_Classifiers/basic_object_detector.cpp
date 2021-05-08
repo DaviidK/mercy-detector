0011589e000fcf91bddca74c3b8118079365be08
David Kang <49457408+DaviidK@users.noreply.github.com>
2021-05-07 23:27:14 -0700

    Made minor formatting changes

--- Tutorials/Object_Detection_Using_Cascade_Classifiers/basic_object_detector.cpp
+++ Tutorials/Object_Detection_Using_Cascade_Classifiers/basic_object_detector.cpp
@@ -18,8 +18,7 @@ using namespace cv;
 
 void detectAndDisplay(CascadeClassifier& face_cascade, CascadeClassifier& eyes_cascade, Mat frame);
 
-int main(int argc, const char** argv)
-{
+int main(int argc, const char** argv) {
     CascadeClassifier face_cascade;
     CascadeClassifier eyes_cascade;
 
@@ -33,66 +32,66 @@ int main(int argc, const char** argv)
     parser.printMessage();
     String face_cascade_name = samples::findFile(parser.get<String>("face_cascade"));
     String eyes_cascade_name = samples::findFile(parser.get<String>("eyes_cascade"));
+    
     //-- 1. Load the cascades
-    if (!face_cascade.load(face_cascade_name))
-    {
+    if (!face_cascade.load(face_cascade_name)) {
         cout << "--(!)Error loading face cascade\n";
         return -1;
-    };
-    if (!eyes_cascade.load(eyes_cascade_name))
-    {
+    }
+
+    if (!eyes_cascade.load(eyes_cascade_name)) {
         cout << "--(!)Error loading eyes cascade\n";
         return -1;
-    };
+    }
+
     int camera_device = parser.get<int>("camera");
     VideoCapture capture;
+
     //-- 2. Read the video stream
     capture.open(camera_device);
-    if (!capture.isOpened())
-    {
+    if (!capture.isOpened()) {
         cout << "--(!)Error opening video capture\n";
         return -1;
     }
+
     Mat frame;
-    while (capture.read(frame))
-    {
-        if (frame.empty())
-        {
+    while (capture.read(frame)) {
+        if (frame.empty()) {
             cout << "--(!) No captured frame -- Break!\n";
             break;
         }
+
         //-- 3. Apply the classifier to the frame
         detectAndDisplay(frame);
-        if (waitKey(10) == 27)
-        {
+        if (waitKey(10) == 27) {
             break; // escape
         }
     }
     return 0;
 }
-void detectAndDisplay(Mat frame)
-{
+
+void detectAndDisplay(Mat frame) {
     Mat frame_gray;
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
     equalizeHist(frame_gray, frame_gray);
     //-- Detect faces
     std::vector<Rect> faces;
     face_cascade.detectMultiScale(frame_gray, faces);
-    for (size_t i = 0; i < faces.size(); i++)
-    {
+
+    for (size_t i = 0; i < faces.size(); i++) {
         Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
         ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
         Mat faceROI = frame_gray(faces[i]);
         //-- In each face, detect eyes
         std::vector<Rect> eyes;
         eyes_cascade.detectMultiScale(faceROI, eyes);
-        for (size_t j = 0; j < eyes.size(); j++)
-        {
+        for (size_t j = 0; j < eyes.size(); j++) {
             Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
             int radius = cvRound((eyes[j].width + eyes[j].height) * 0.25);
             circle(frame, eye_center, radius, Scalar(255, 0, 0), 4);
         }
     }
+
     //-- Show what you got
     imshow("Capture - Face detection", frame);
 }
