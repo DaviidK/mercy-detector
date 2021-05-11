8c0d5486d8e261e1dbabfdb10e943bd06981e77a
Sana Suse <sana96@uw.edu>
2021-05-10 21:19:49 -0700

    Start on grabcut for preprocessing template images

---
+++ Tutorials/Grabcut/grabcut_to_mask.cpp
@@ -0,0 +1,36 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+Mat removeBackground(Mat grabcutImg, Mat result) {
+	int pixelBVal, pixelGVal, pixelRVal;
+	for (int r = 0; r < grabcutImg.rows; r++) {
+		for (int c = 0; c < grabcutImg.cols; c++) {
+			pixelBVal = grabcutImg.at<Vec3b>(r, c)[0];
+			pixelGVal = grabcutImg.at<Vec3b>(r, c)[1];
+			pixelRVal = grabcutImg.at<Vec3b>(r, c)[2];
+			if (pixelBVal == 0 && pixelGVal == 0 && pixelRVal == 0) {
+				result.at<uchar>(r, c) = 0;
+			}
+			else {
+				result.at<uchar>(r, c) = 0;
+			}
+		}
+	}
+	return result;
+}
+
+int main(int argc, char* argv[]) {
+	Mat grabcutImg = imread("Detection_Algorithm/Data/Static_Test_Im/template_grabcut_mercy.jpg");
+	imshow("Grab", grabcutImg);
+	waitKey(0);
+
+	Mat temp = Mat(grabcutImg.rows, grabcutImg.cols, CV_8U);
+	Mat result = removeBackground(grabcutImg, temp);
+	imshow("Show", result);
+
+	waitKey(0);
+
+}
