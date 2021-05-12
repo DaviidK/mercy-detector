3dd29247f30e4a7fe577e4fb7e131b58617622fc
Matthew Munson <mmunson11@gmail.com>
2021-05-11 22:08:13 -0700

    Added Video Frame Extractor and Video Files
    • Created a Tools directory for code that we use to preprocess our data
    
    • Started on the video_frame_extractor tool
    
    • Brought videos into the directory. Removed anything over 100 MB so the walking videos are missing for now.

---
+++ Tools/video_frame_extractor.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    string directory = "Detection_Algorithm/Data/Video/Mercy/Wand/idle.mp4";
+
+
+}
+
