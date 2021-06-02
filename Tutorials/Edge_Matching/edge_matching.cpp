78384168c5cb75a13c102db65099288e668fdd31
Irene W <i.lalinw@gmail.com>
2021-06-01 21:58:16 -0700

    refactored methods, added basic comments

--- Tutorials/Edge_Matching/edge_matching.cpp
+++ Tutorials/Edge_Matching/edge_matching.cpp
@@ -25,11 +25,9 @@
 using namespace cv;
 using namespace std;
 
-const String INPUT_FILE = "Detection_Algorithm/Data/Static_Test_Im/busy.jpg";
+const String INPUT_FILE = "Detection_Algorithm/Data/Static_Test_Im/template.jpg";
 const String TEMPLATE_FILE = "Detection_Algorithm/Data/Static_Test_Im/template.jpg";
 const int THRESHOLD = 100;
-Mat input_gray;
-Mat template_gray;
 RNG rng(98765);
 
 
@@ -70,7 +68,7 @@ int distanceFromTo(const vector<Point>& a, const vector<Point>& b)
         }
         maxDistAB += minB;
     }
-    //cout << "Total Distance = " << maxDistAB << endl;
+    cout << "(Total Distance) = " << maxDistAB << endl;
     return maxDistAB;
 }
 
@@ -90,6 +88,65 @@ double distance_hausdorff(const vector<Point>& a, const vector<Point>& b)
 }
 
 
+/************************************************************************************************
+*   showContours
+*   draws contours on an empty image, has option to use random colors (false for white)
+*************************************************************************************************/
+bool showContours(Mat image_canny, vector<vector<Point>> contours, bool randomColors) {
+    Mat image_drawing = Mat::zeros(image_canny.size(), CV_8UC3);
+    for (size_t i = 0; i < contours.size(); i++)
+    {
+        if (randomColors) {
+            Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+            drawContours(image_drawing, contours, (int)i, color);
+        }
+        else { 
+            //white contours
+            drawContours(image_drawing, contours, (int)i, Scalar(255, 255, 255));
+        }
+        
+    }
+    cout << "Edge map created." << endl;
+    imshow("Edge Map", image_drawing);
+    waitKey();
+
+    return true;
+}
+
+
+/************************************************************************************************
+*   createEdgeMap
+*   create edge map of image passed into it, has option to show drawn image
+*************************************************************************************************/
+vector<vector<Point>> createEdgeMap(Mat image, bool showImage, bool randomColors) {
+    //Create edge map of image
+    Mat image_gray, image_canny;
+
+    cvtColor(image, image_gray, COLOR_BGR2GRAY);
+    blur(image_gray, image_gray, Size(3, 3));
+    Canny(image_gray, image_canny, THRESHOLD, THRESHOLD * 2);
+    vector<vector<Point> > contours;
+    findContours(image_canny, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+
+    if (showImage) {
+        showContours(image_canny, contours, true);
+    }
+
+    return contours;
+}
+
+/************************************************************************************************
+*   poolPoints
+*   pull all points in a collection of vectors into one pool of points 
+*************************************************************************************************/
+vector<Point> poolPoints(vector<vector<Point>> contours) {
+    vector<Point> points_pool = {};
+    for (int i = 0; i < contours.size(); i++) {
+        points_pool.insert(points_pool.end(), contours[i].begin(), contours[i].end());
+    }
+    return points_pool;
+}
+
 
 /************************************************************************************************
 *   MAIN Method
@@ -107,60 +164,25 @@ int main(int argc, char** argv)
     }
     cout << "Template and input file loaded successfully." << endl;
 
+    //Create edge maps for image
     cout << "Creating edge maps..." << endl;
+    vector<vector<Point>> template_contours = createEdgeMap(templt, false, true);
+    vector<vector<Point>> input_contours = createEdgeMap(input, false, true);
 
-    //Create edge map of TEMPLATE 
-    cvtColor(templt, template_gray, COLOR_BGR2GRAY);
-    blur(template_gray, template_gray, Size(3, 3));
-    Mat template_canny;
-    Canny(template_gray, template_canny, THRESHOLD, THRESHOLD * 2);
-    vector<vector<Point> > template_contours;
-    findContours(template_canny, template_contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-    /*Mat template_drawing = Mat::zeros(template_canny.size(), CV_8UC3);
-    for (size_t i = 0; i < template_contours.size(); i++)
-    {
-        Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-        drawContours(template_drawing, template_contours, (int)i, color);
-    }
-    cout << "Template image edge map created." << endl;
-    imshow("Template", template_drawing);
-    waitKey();*/
-
-    //Create edge map of INPUT 
-    cvtColor(input, input_gray, COLOR_BGR2GRAY);
-    blur(input_gray, input_gray, Size(3, 3));
-    Mat input_canny;
-    Canny(input_gray, input_canny, THRESHOLD, THRESHOLD * 2);
-    vector<vector<Point> > input_contours;
-    findContours(input_canny, input_contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-    /*Mat input_drawing = Mat::zeros(input_canny.size(), CV_8UC3);
-    for (size_t i = 0; i < input_contours.size(); i++)
-    {
-        Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-        drawContours(input_drawing, input_contours, (int)i, color);
-    }
-    cout << "Input image edge map created." << endl;
-    imshow("Input", template_drawing);
-    waitKey();*/
-
-    cout << "Pooling template image points..." << endl;
-    vector<Point> template_points_pool = {};
-    for (int i = 0; i < template_contours.size(); i++) {
-        template_points_pool.insert(template_points_pool.end(), template_contours[i].begin(), template_contours[i].end());
-    }
-
-    cout << "Pooling input image points..." << endl;
-    vector<Point> input_points_pool = {};
-    for (int i = 0; i < input_contours.size(); i++) {
-        input_points_pool.insert(input_points_pool.end(), input_contours[i].begin(), input_contours[i].end());
-    }
-
-    cout << "Calculating Hausdorff Distance with internal method..." << endl;
+    //Pool points from contours for hausdorff distance calculations, start timer
+    cout << "Calculating Hausdorff Distance with internal method... (timer start!)" << endl;
+    cout << "Pooling image points..." << endl;
     chrono::steady_clock::time_point start = chrono::steady_clock::now();
+    vector<Point> template_points_pool = poolPoints(template_contours);
+    vector<Point> input_points_pool = poolPoints(input_contours);
+
+    //Calculate Hausdorff Distance, records time 
     double distance_hausdorff_internal = distance_hausdorff(template_points_pool, input_points_pool);
+    
+    //Stop timer and show elapsed time
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
     int time = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    cout << "[Elapsed time in milliseconds: " << time << " ms]" << endl;
+    cout << "[Hausdorff Distance caluclation time: " << time << " ms]" << endl;
 
     waitKey(5000); //wait for 5 sec
 
