d70d13839017586fd929e6164a9586989279ed9f
Irene W <i.lalinw@gmail.com>
2021-06-01 20:38:19 -0700

    add elapsed time

--- Tutorials/Edge_Matching/edge_matching.cpp
+++ Tutorials/Edge_Matching/edge_matching.cpp
@@ -1,67 +1,58 @@
-// Tutorials for findContours() and convexHull()
-// press any key to move to the next test method window
+/************************************************************************************************
+* Edge Matching with Haudorff Distance
+* 
+* @author: Irene Wachirawutthichai
+* @date: May 25 2021
+* 
+* 
+* references: 
+* - https://programmersought.com/article/14824834608/
+* - https://stackoverflow.com/questions/21482534/how-to-use-shape-distance-and-common-interfaces-to-find-hausdorff-distance-in-op
+* - https://titanwolf.org/Network/Articles/Article?AID=813ecc86-ae22-4844-bd2b-0e516f9d15ce#gsc.tab=0
+* 
+* 
+*
+************************************************************************************************/
 
 #include <opencv2/core.hpp>
+#include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/shape/shape_distance.hpp>
 #include <iostream>
+#include <chrono>
 
 using namespace cv;
 using namespace std;
 
-const String INPUT_FILE = "Detection_Algorithm/Data/Static_Test_Im/busy.jpg";
-Mat src_gray;
-int thresh = 100;
-RNG rng(12345);
-const char* source_window = "Source";
+const String INPUT_FILE = "Detection_Algorithm/Data/Static_Test_Im/light.jpg";
+const String TEMPLATE_FILE = "Detection_Algorithm/Data/Static_Test_Im/template.jpg";
+const int THRESHOLD = 100;
+Mat input_gray;
+Mat template_gray;
+RNG rng(98765);
 
-// Helper to display image with a scale factor parameter for resizing
+
+/************************************************************************************************
+*   displayImage
+*   Helper to display image with a scale factor parameter for resizing
+* 
+************************************************************************************************/
 void displayImage(Mat image, String windowName, int scaleFactor) {
     namedWindow(windowName, WINDOW_NORMAL);
     resizeWindow(windowName, image.cols / scaleFactor, image.rows / scaleFactor);
     imshow(windowName, image);
 }
 
-//void convex_thresh_callback(int, void*)
-//{
-//    Mat canny_output;
-//    Canny(src_gray, canny_output, thresh, thresh * 2);
-//    vector<vector<Point> > contours;
-//    findContours(canny_output, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
-//    vector<vector<Point> >hull(contours.size());
-//    for (size_t i = 0; i < contours.size(); i++)
-//    {
-//        convexHull(contours[i], hull[i]);
-//    }
-//    Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
-//    for (size_t i = 0; i < contours.size(); i++)
-//    {
-//        Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-//        drawContours(drawing, hull, (int)i, color, 2);
-//    }
-//    displayImage(drawing, source_window, 1);
-//}
 
 
-
-void contour_thresh_callback(int, void*)
-{
-    Mat canny_output;
-    Canny(src_gray, canny_output, thresh, thresh * 2);
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-    Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
-    for (size_t i = 0; i < contours.size(); i++)
-    {
-        Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-        drawContours(drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
-    }
-    displayImage(drawing, source_window, 1);
-}
-
-// internal helper for HAUSDORFF DISTANCE
+/************************************************************************************************
+*   distanceFromTo 
+*   internal helper for HAUSDORFF DISTANCE
+* 
+* 
+************************************************************************************************/
 int distanceFromTo(const vector<Point>& a, const vector<Point>& b)
 {
     int maxDistAB = 0;
@@ -71,20 +62,22 @@ int distanceFromTo(const vector<Point>& a, const vector<Point>& b)
             int dx = (a[i].x - b[j].x);
             int dy = (a[i].y - b[j].y);
             int tmpDist = dx * dx + dy * dy;
-
             if (tmpDist < minB) {
                 minB = tmpDist;
             }
-
             if (tmpDist == 0) {
                 break; // can't get better than equal.
             }
         }
         maxDistAB += minB;
     }
+    cout << "Total Distance = " << maxDistAB << endl;
     return maxDistAB;
 }
 
+/************************************************************************************************
+*   distance_hausdorff
+*************************************************************************************************/
 double distance_hausdorff(const vector<Point>& a, const vector<Point>& b)
 {
     int maxDistAB = distanceFromTo(a, b);
@@ -97,36 +90,79 @@ double distance_hausdorff(const vector<Point>& a, const vector<Point>& b)
     return result;
 }
 
+
+
+/************************************************************************************************
+*   MAIN Method
+*************************************************************************************************/
 int main(int argc, char** argv)
 {
-    //read single frame source
-    Mat src = imread(INPUT_FILE);
-    if (src.empty())
+    //Read INPUT file and TEMPLATE file from source
+    Mat input = imread(INPUT_FILE);
+    Mat templt = imread(TEMPLATE_FILE);
+    if (input.empty() || templt.empty() )
     {
         cout << "Could not open or find the image!\n" << endl;
         cout << "Usage: " << argv[0] << " <Input image>" << endl;
         return -1;
     }
+    cout << "Template and input file loaded successfully." << endl;
 
-    //get canny edge
-    cvtColor(src, src_gray, COLOR_BGR2GRAY);
-    blur(src_gray, src_gray, Size(3, 3));
-    namedWindow(source_window);
-    displayImage(src, source_window, 1);
-    const int max_thresh = 255;
-    //trackbar for canny edge
-    createTrackbar("Canny thresh:", source_window, &thresh, max_thresh, convex_contour_thresh_callback);
-    convex_contour_thresh_callback(0, 0);
+    cout << "Creating edge maps..." << endl;
+
+    //Create edge map of TEMPLATE 
+    cvtColor(templt, template_gray, COLOR_BGR2GRAY);
+    blur(template_gray, template_gray, Size(3, 3));
+    Mat template_canny;
+    Canny(template_gray, template_canny, THRESHOLD, THRESHOLD * 2);
+    vector<vector<Point> > template_contours;
+    findContours(template_canny, template_contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
+    /*Mat template_drawing = Mat::zeros(template_canny.size(), CV_8UC3);
+    for (size_t i = 0; i < template_contours.size(); i++)
+    {
+        Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+        drawContours(template_drawing, template_contours, (int)i, color);
+    }
+    cout << "Template image edge map created." << endl;
+    imshow("Template", template_drawing);
+    waitKey();*/
+
+    //Create edge map of INPUT 
+    cvtColor(input, input_gray, COLOR_BGR2GRAY);
+    blur(input_gray, input_gray, Size(3, 3));
+    Mat input_canny;
+    Canny(input_gray, input_canny, THRESHOLD, THRESHOLD * 2);
+    vector<vector<Point> > input_contours;
+    findContours(input_canny, input_contours, RETR_LIST, CHAIN_APPROX_SIMPLE); 
+    /*Mat input_drawing = Mat::zeros(input_canny.size(), CV_8UC3);
+    for (size_t i = 0; i < input_contours.size(); i++)
+    {
+        Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+        drawContours(input_drawing, input_contours, (int)i, color);
+    }
+    cout << "Input image edge map created." << endl;
+    imshow("Input", template_drawing);
+    waitKey();*/
+  
+    cout << "Pooling template image points..." << endl;
+    vector<Point> template_points_pool = {}; 
+    for (int i = 0; i < template_contours.size(); i++) {
+        template_points_pool.insert(template_points_pool.end(), template_contours[i].begin(), template_contours[i].end());
+    }
+
+    cout << "Pooling input image points..." << endl;
+    vector<Point> input_points_pool = {};
+    for (int i = 0; i < input_contours.size(); i++) {
+        input_points_pool.insert(input_points_pool.end(), input_contours[i].begin(), input_contours[i].end());
+    }
+
+    cout << "Calculating Hausdorff Distance with internal method..." << endl;
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+    double distance_hausdorff_internal = distance_hausdorff(template_points_pool, input_points_pool);
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+
+    cout << "[Elapsed time in milliseconds: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms]" << endl;
     waitKey();
 
-
-    //read stream of frames from VIDEO source
-
-    //calculate HAUSDORFF distance 
-
-    //print distance 
-    //compare distance with template 
-
-    
     return 0;
 }
