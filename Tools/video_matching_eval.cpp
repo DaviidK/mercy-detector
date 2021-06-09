02515ccb44b9b7a1d5b49c2aac689f06b595a3c5
unknown <49457408+DaviidK@users.noreply.github.com>
2021-06-09 12:52:36 -0700

    Updated the video_matching_eval class to contain the code used in cascade classifier evaluation. Also added documentation for all methods

--- Tools/video_matching_eval.cpp
+++ Tools/video_matching_eval.cpp
@@ -27,8 +27,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "Detection_Algorithm/Src/Overwatch_Constants/overwatchConstants.h"
-#include "Tutorials/Template_Matching/template_matching.h"
-#include "Detection_Algorithm/Src/classifier_detector/classifier_detector.h"
+#include "Detection_Algorithm/Src/Template_Matching/template_matching.h"
+#include "Detection_Algorithm/Src/Classifier_Detector/classifier_detector.h"
 #include "Tools/CSV/csv_wrapper.h"
 #include "Tools/Meta_File/meta_file.h"
 
@@ -42,9 +42,14 @@ using std::chrono::system_clock;
 
 static const string VIDEO_FILE_PATHS = "Detection_Algorithm/Data/Video/video_paths.csv";
 static const string VIDEO_FILE_PREFIX = "Detection_Algorithm/Data/Video/";
-static const string DETECTION_TYPES[] = { "Template-Matching", "Cascade-Classifier", "Edge-Matching" };
+static const string DETECTION_TYPES[] = { "Template-Matching", "Cascade-Classifier" };
 static const int DETECTION_METHOD = 0;
-static const bool USE_META_FILE = false;
+static const bool USE_META_FILE = true;
+
+// Template matching specific parameters
+static const int NUM_MATCHING_METHODS = 8;
+static const int MATCH_METHOD = 2;
+static const bool USE_MASK = false;
 
 // Template matching specific parameters
 static const int NUM_MATCHING_METHODS = 8;
@@ -56,12 +61,20 @@ void processVideoTemplateMatching(VideoCapture capture,
 	vector<vector<string>>& output,
 	string filePath);
 
-void processMetaTemplateMatching(VideoCapture capture, MetaFile& metaFile, vector<vector<string>>& output, string videoPath);
+void processMetaTemplateMatching(VideoCapture capture, 
+	MetaFile& metaFile, 
+	vector<vector<string>>& output, 
+	string videoPath);
 
-void processVideoCascadeClassifier(VideoCapture capture,
-							       OWConst::Heroes expectedHero,
-								   vector<vector<string>>& output,
-								   string filepath);
+void processVideoCascadeClassifier(VideoCapture capture, 
+	OWConst::Heroes expectedHero,
+	vector<vector<string>>& output, 
+	string filepath);
+
+void processMetaCascadeClassifier(VideoCapture capture, 
+	MetaFile& metaFile, 
+	vector<vector<string>>& output, 
+	string videoPath);
 
 void displayStats(const int& correct, const int& total);
 
@@ -79,6 +92,9 @@ string getDateTime();
  * - Waits for minimum amount of time and displays the frame
  * - Records the time at the end of the loop
  *
+ * @pre: VIDEO_FILE_PATHS contains filepaths to all videos to be evaluated
+ * @post: A CSV file will be generated containing results of object detection for the selected method
+ * 				through all video files evaluated. 
  **************************************************************************************************/
 int main() {
 	vector<vector<string>> videoFiles;
@@ -132,11 +148,12 @@ int main() {
 			}
 		}
 		else if (DETECTION_METHOD == 1) {
-			processVideoCascadeClassifier(capture, expectedHero, output, shortPath);
-		}
-		else if (DETECTION_METHOD == 2) {
-			// TODO: Edge Matching frame processing method here
-			// Pass in capture, expectedHero and output.
+			if (USE_META_FILE) {
+				processMetaCascadeClassifier(capture, metaFile, output, shortPath);
+			}
+			else {
+				processVideoCascadeClassifier(capture, expectedHero, output, shortPath);
+			}
 		}
 		else {
 			cout << "The given detection method is not one of the 3 accepted for this program." << endl;
@@ -157,7 +174,16 @@ int main() {
  * This method processes a given VideoCapture to detect heroes in each frame.
  * It stores the resulting correct counts and total number of frames into a csv file for easy data
  * processing.
- *
+ * 
+ * @param capture: An input video which will be evaluated
+ * @param expectedHero: The known hero which is present through the entire input video
+ * @param output: A 2D vector of strings which will save output results for object detection
+ * @param filepath: The string filepath where the evaluated video is stored
+ * 
+ * @pre: A video containing gameplay of one specific hero is stored at the parameter filepath.
+ * @post: A single row of output will be added to the passed parameter string. This output will 
+ *        contain details of object detection results, including the number of frames correctly 
+ *        identified and the total number of frames.
  **************************************************************************************************/
 void processVideoTemplateMatching(VideoCapture capture, OWConst::Heroes expectedHero,
 	vector<vector<string>>& output, string filepath) {
@@ -234,11 +260,21 @@ void processVideoTemplateMatching(VideoCapture capture, OWConst::Heroes expected
  * This method processes a given VideoCapture to detect heroes in each frame. Unlike the previous
  * template matching video processing video, the video received in this method does not have one
  * single hero or action that each frame corresponds to. Therefore, it uses a metafile that
- * describes the state of each frame of the video to
- *
- *
+ * describes the state of each frame of the video.
+ * 
+ * @param capture: An input video which will be evaluated
+ * @param metaFile: The metafile data which contains information regarding each frame of the video
+ * @param output: A 2D vector of strings which will save output results for object detection
+ * @param filepath: The string filepath where the evaluated video is stored
+ * 
+ * @pre: A video containing gameplay is stored at the given filepath, and the passed parameter 
+ * 		   metafile contains data correctly identifying the state of the video at each frame.
+ * @post: A single row of output will be added to the passed parameter string. This output will 
+ *        contain details of object detection results, including the number of frames correctly 
+ *        identified for both hero and weapon, and the total number of frames.
  **************************************************************************************************/
-void processMetaTemplateMatching(VideoCapture capture, MetaFile& metaFile, vector<vector<string>>& output, string videoPath) {
+void processMetaTemplateMatching(VideoCapture capture, MetaFile& metaFile, 
+																 vector<vector<string>>& output, string videoPath) {
 	Mat frame;
 	int frameCount = 0;
 	int evalFrameCount = 0;
@@ -336,11 +372,193 @@ void processMetaTemplateMatching(VideoCapture capture, MetaFile& metaFile, vecto
 }
 
 /***************************************************************************************************
+ * Process Video for Cascade Classifiers
+ *
+ * This method processes a given VideoCapture to detect heroes in each frame using cascade classifiers.
+ * It stores the resulting correct counts and total number of frames into a csv file for easy data
+ * processing.
+ * 
+ * @param capture: An input video which will be evaluated
+ * @param expectedHero: The known hero which is present through the entire input video
+ * @param output: A 2D vector of strings which will save output results for object detection
+ * @param filepath: The string filepath where the evaluated video is stored
+ * 
+ * @pre: A video containing gameplay of one specific hero is stored at the parameter filepath.
+ * @post: A single row of output will be added to the passed parameter string. This output will 
+ *        contain details of object detection results, including the number of frames correctly 
+ *        identified and the total number of frames.
+ **************************************************************************************************/
+void processVideoCascadeClassifier(VideoCapture capture, OWConst::Heroes expectedHero,
+								   vector<vector<string>>& output, string filepath) {
+	Mat frame;
+	int correctCount = 0;
+	int totalFrameCount = 0;
+
+	cout << "Progress (* per 50 frame): " << endl;
+	int match_method; bool use_mask;
+
+	vector<OWConst::Heroes> knownHero = { expectedHero };
+	classifier_detector CCDetector;
+
+	capture >> frame;
+	while (!frame.empty()) {
+		if (totalFrameCount % 50 == 0) {
+			cout << "*";
+		}
+
+		// Crop the source image so it only looks at bottom right quarter of screen
+		Rect newSize = Rect(frame.cols / 2, frame.rows / 2, frame.cols / 2, frame.rows / 2);
+		Mat croppedImage = frame(newSize);
+
+		if (CCDetector.evaluateHeroClassifier(croppedImage, expectedHero)) {
+			correctCount++;
+		}
+
+		totalFrameCount++;
+		capture >> frame;
+	}
+	if (output.size() == 0) {
+		vector<string> colNames;
+		colNames.push_back("videoPath");
+		colNames.push_back("expectedHero");
+		colNames.push_back("heroCorrect");
+		colNames.push_back("totalFrames");
+		output.push_back(colNames);
+	}
+
+	vector<string> row;
+	row.push_back(filepath);
+	row.push_back(OWConst::getHeroString(expectedHero));
+	row.push_back(to_string(correctCount));
+	row.push_back(to_string(totalFrameCount));
+	output.push_back(row);
+
+	displayStats(correctCount, totalFrameCount);
+	cout << endl;
+}
+
+/***************************************************************************************************
+ * Process Video for Cascde Classifiers using the video metafile
+ *
+ * This method processes a given VideoCapture to detect heroes and weapon actions in each frame using
+ * cascade classifiers. The video received in this method may contain multiple different heroes.
+ * Therefore, it uses a metafile that describes the state of each frame of the video.
+ *
+ * @param capture: An input video which will be evaluated
+ * @param metaFile: The metafile data which contains information regarding each frame of the video
+ * @param output: A 2D vector of strings which will save output results for object detection
+ * @param filepath: The string filepath where the evaluated video is stored
+ * 
+ * @pre: A video containing gameplay is stored at the given filepath, and the passed parameter 
+ * 		   metafile contains data correctly identifying the state of the video at each frame.
+ * @post: A single row of output will be added to the passed parameter string. This output will 
+ *        contain details of object detection results, including the number of frames correctly 
+ *        identified for both hero and weapon, and the total number of frames.
+ **************************************************************************************************/
+void processMetaCascadeClassifier(VideoCapture capture, MetaFile& metaFile, 
+																  vector<vector<string>>& output, string videoPath) {
+	Mat frame;
+	int frameCount = 0;
+	int evalFrameCount = 0;
+	OWConst::Heroes expectedHero;
+	OWConst::Heroes detectedHero;
+	OWConst::WeaponActions expectedAction;
+	OWConst::WeaponActions detectedAction;
+
+	classifier_detector CCDetector;
+
+	int heroCorrectCt = 0;
+	int heroIncorrectCt = 0;
+	int actionCorrectCt = 0;
+	int actionIncorrectCt = 0;
+
+	cout << "Progress (* per 50 frame): " << endl;
+
+	while (true) {
+		capture >> frame;
+		frameCount++;
+		
+		if (frame.empty()) {
+			break;
+		}
+
+		if (frameCount % 50 == 0) {
+			cout << "*";
+		}
+
+		if (frameCount % 5 != 0) {
+			continue;
+		}
+
+
+		expectedHero = metaFile.getHero(frameCount);
+		expectedAction = metaFile.getWeaponAction(frameCount);
+
+		detectedHero = CCDetector.identifyHero(frame);
+
+		// Assume correct hero was detected to detect the weapon and limit the classifiers used
+		detectedAction = CCDetector.identifyAction(frame);
+
+		if (detectedHero == expectedHero) {
+			heroCorrectCt++;
+		}
+		else {
+			heroIncorrectCt++;
+		}
+
+		// To simplify the process, we are currently just looking at whether the staff or the 
+		// pistol is being held by Mercy.
+		if (expectedAction == OWConst::Damage_Boosting ||
+			expectedAction == OWConst::Healing) {
+			expectedAction = OWConst::Holding_Staff;
+		}
+		else if (expectedAction == OWConst::Firing) {
+			expectedAction = OWConst::Holding_Pistol;
+		}
+
+		if (expectedAction == detectedAction) {
+			actionCorrectCt++;
+		}
+		else {
+			actionIncorrectCt++;
+		}
+
+		evalFrameCount++;
+	}
+
+	if (output.size() == 0) {
+		vector<string> colNames;
+		colNames.push_back("videoPath");
+		colNames.push_back("heroCorrect");
+		colNames.push_back("heroIncorrect");
+		colNames.push_back("actionCorrect");
+		colNames.push_back("actionIncorrect");
+		colNames.push_back("totalFrames");
+		output.push_back(colNames);
+	}
+
+	vector<string> row;
+	row.push_back(videoPath);
+	row.push_back(to_string(heroCorrectCt));
+	row.push_back(to_string(heroIncorrectCt));
+	row.push_back(to_string(actionCorrectCt));
+	row.push_back(to_string(actionIncorrectCt));
+	row.push_back(to_string(evalFrameCount));
+
+	output.push_back(row);
+}
+
+/***************************************************************************************************
  * Display Stats
  *
  * This method takes in a correct count and a total number of frames count. Using this, it prints
  * out what those values are, and as a percentage returns the effectiveness of the detection method.
  *
+ * @param correct: The number of frames correctly identified
+ * @param total: The total number of frames
+ * 
+ * @pre: A video has been evaluated using either template matching and the results have been recorded
+ * @post: The correct and total counts will be output to the user via the terminal
  **************************************************************************************************/
 void displayStats(const int& correct, const int& total) {
 	double effectiveness = static_cast<double>(correct) / total * 100.0;
@@ -356,6 +574,10 @@ void displayStats(const int& correct, const int& total) {
  * This helper method retrieves the current time.
  * This can be used to help name the outputted csv files.
  *
+ * @pre: 
+ * @post: The date and time will be recorded and returned to wherever this method was called
+ * 
+ * @return: The current date and local time in the form dayMonthYear-hourMinuteSecond as a string
  **************************************************************************************************/
 string getDateTime() {
 	time_t now;
