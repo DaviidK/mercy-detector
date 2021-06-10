71e1d826dc47c1a34487f4b242b8b592368f06e9
Irene W <i.lalinw@gmail.com>
2021-06-09 18:35:17 -0700

    update edge_processing documentation

--- Tools/Edge_Processing/edge_processing.h
+++ Tools/Edge_Processing/edge_processing.h
@@ -1,3 +1,13 @@
+/***************************************************************************************************
+* Image processer tool - Edge Map
+*
+* @author: Irene Wachirawutthichai
+* @date: 06/03/2021
+*
+* General tool for generating edge maps
+*
+***************************************************************************************************/
+
 #include <opencv2/core.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -9,4 +19,19 @@
 using namespace cv;
 using namespace std;
 
+
+/***************************************************************************************************
+* Create Edge Map
+*
+* This method takes an image as an input, then creates an edge map of the same size and
+* returns the edge map as a new object
+*
+* @param    Mat& input_image  : Mat object of input image
+*
+* @pre      Input Mat received is valid
+* @post     Edge map is created based on the input image and returned
+*
+* @return   Edge map of input image as Mat object
+*
+***************************************************************************************************/
 Mat createEdgeMap(Mat& input_image);
